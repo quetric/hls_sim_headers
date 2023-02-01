1419c381a46f56044d6b12da1f159ad5237d12a4
Lucian Petrica <lpetrica@lpetrica-laptop>
2023-02-01 19:23:38 +0000

    Collected headers

---
+++ include/ap_axi_sdata.h
@@ -0,0 +1,90 @@
+// (C) Copyright 2016-2021 Xilinx, Inc.
+// All Rights Reserved.
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// (C) Copyright 2016-2021 Xilinx, Inc.
+// All Rights Reserved.
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+
+
+/*
+ * This file contains the definition of the data types for AXI streaming. 
+ * ap_axi_s is a signed interpretation of the AXI stream
+ * ap_axi_u is an unsigned interpretation of the AXI stream
+ */
+
+#pragma once
+
+#include "ap_int.h"
+
+template<int D,int U,int TI,int TD>
+  struct ap_axis{
+    ap_int<D>        data;
+    ap_uint<(D+7)/8> keep;
+    ap_uint<(D+7)/8> strb;
+    ap_uint<U>       user;
+    ap_uint<1>       last;
+    ap_uint<TI>      id;
+    ap_uint<TD>      dest;
+  };
+
+template<int D>
+  struct ap_axis <D, 0, 0, 0>{
+    ap_int<D>        data;
+    ap_uint<(D+7)/8> keep;
+    ap_uint<(D+7)/8> strb;
+    ap_uint<1>       last;
+  };
+
+template<int D,int U,int TI,int TD>
+  struct ap_axiu{
+    ap_uint<D>       data;
+    ap_uint<(D+7)/8> keep;
+    ap_uint<(D+7)/8> strb;
+    ap_uint<U>       user;
+    ap_uint<1>       last;
+    ap_uint<TI>      id;
+    ap_uint<TD>      dest;
+  };
+
+template<int D>
+  struct ap_axiu <D, 0, 0, 0>{
+    ap_uint<D>       data;
+    ap_uint<(D+7)/8> keep;
+    ap_uint<(D+7)/8> strb;
+    ap_uint<1>       last;
+  };
+
